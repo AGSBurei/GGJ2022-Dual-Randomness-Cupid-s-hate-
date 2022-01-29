0e586da44fd12c7adee111016864427d1608a147
Burei <rommanirs.mer@outlook.fr>
2022-01-29 20:17:38 +0100

    wip bow shoot

--- Assets/Scripts/Weapon/Arrow.cs
+++ Assets/Scripts/Weapon/Arrow.cs
@@ -1,13 +1,28 @@
+using Platformer.Mechanics;
 using UnityEngine;
 
-namespace Object
+namespace Weapon
 {
     public class Arrow : MonoBehaviour
 
     {
-        [SerializeField]
-        public string color;
+        [SerializeField] private string color;
+
+        [SerializeField] private GameObject player;
+        public PlayerController playerController;
+        private Vector2 _position;
+
+        public void Start()
+        {
+            Transform playerTransform = player.transform;
+            _position = playerTransform.position;
+            transform.position = new Vector2(_position.x, _position.y);
+            playerController = player.GetComponent<PlayerController>();
+        }
         
-        
+        public string GetColor()
+        {
+            return color;
+        }
     }
 }
