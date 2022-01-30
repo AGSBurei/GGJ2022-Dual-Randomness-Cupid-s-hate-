fb007780977429622852b0e7fb4942822889945b
Burei <rommanirs.mer@outlook.fr>
2022-01-30 11:01:49 +0100

    functionnal arrow spending

--- Assets/Scripts/Weapon/Bow.cs
+++ Assets/Scripts/Weapon/Bow.cs
@@ -34,13 +34,7 @@ namespace Weapon
         public void Shoot(String currentArrow)
         {
             RemoveArrowFromQuiver(currentArrow);
-            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
-
-            if (hit.collider != null)
-            {
-                Debug.Log(hit.collider.name);
-            }
+            Instantiate();
         }
     }
 }
--- Assets/Scripts/Weapon/Quiver.cs
+++ Assets/Scripts/Weapon/Quiver.cs
@@ -12,7 +12,7 @@ namespace Weapon
         [SerializeField] public int minArrow;
         [SerializeField] public int maxArrow;
         [SerializeField] public ArrowSelector arrowSelector;
-        private List<Dictionary<string, int>> _Arrow = new List<Dictionary<string, int>>();
+        private Dictionary<string, int> _Arrow = new Dictionary<string, int>();
 
         public void Awake()
         {
@@ -25,9 +25,7 @@ namespace Weapon
             {
                 if (arrowType != null)
                 {
-                    var dico = new Dictionary<string, int>();
-                    dico.Add(arrowType, Random.Range(minArrow, maxArrow));
-                    _Arrow.Add(dico);
+                    _Arrow.Add(arrowType, Random.Range(minArrow, maxArrow));
                 }
             }
         }
@@ -35,16 +33,14 @@ namespace Weapon
         public int GetArrowCount(string arrowType)
         { 
             var arrowCount = 0;
-            foreach (Dictionary<string, int> arrowList in _Arrow )
+            foreach (KeyValuePair<string, int> arrow in _Arrow)
             {
-                foreach (KeyValuePair<string, int> arrow in arrowList)
+                if (arrow.Key == arrowType)
                 {
-                    if (arrow.Key == arrowType)
-                    {
-                        arrowCount = arrow.Value;
-                    }
+                    arrowCount = arrow.Value;
                 }
             }
+            
             return arrowCount;
         }
         
@@ -53,47 +49,20 @@ namespace Weapon
             var arrowCount = 0;
             var tempArrow = new List<string>();
             
-            
-            foreach (var dictionary in _Arrow.ToList())
+            foreach (var arrow in _Arrow)
             {
-                if (dictionary != null)
+                if (arrow.Key == arrowType)
                 {
-                    foreach (var arrow in dictionary)
-                    {
-                        if (arrow.Key == arrowType)
-                        {
-                            tempArrow.Add(arrow.Key);
-                            arrowCount = arrow.Value - 1;
-                        }
-                    }
+                    tempArrow.Add(arrow.Key);
+                    arrowCount = arrow.Value - 1;
                 }
             }
 
-
-            //foreach (var dictionary in _Arrow.ToList())
-            //{
-            //    if (dictionary != null)
-            //    {
-            //        foreach (var arrow in dictionary)
-            //        {
-            //            if (arrow.Key == arrowType)
-            //            {
-            //                tempArrow.Add(arrow.Key);
-            //                arrowCount = arrow.Value - 1;
-            //            }
-            //        }
-            //    }
-            //}
-            //
-            //
-
-            //foreach (var key in tempArrow)
-            //{
-            //    //dictionary[arrow.Key] = arrowCount;
-            //    
-            //}
+            foreach (var key in tempArrow)
+            {
+                _Arrow[key] = arrowCount;
+            }
         }
-
         public void UpdateArrow(string arrowType)
         {
             SetArrowCount(arrowType);
